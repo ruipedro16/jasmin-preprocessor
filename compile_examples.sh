7ca71444d45b09a0d271aba4e238984aeffaf26b
Rui Fernandes <ruipedro16@protonmail.com>
2023-09-08 09:39:25 +0200

    fix

--- compile_examples.sh
+++ compile_examples.sh
@@ -3,9 +3,9 @@
 mkdir -p out
 
 
-python3 preprocessor.py --input_file examples/example1.jazz --output_file out/example1.jazz
-python3 preprocessor.py --input_file examples/example2.jazz --output_file out/example2.jazz
-python3 preprocessor.py --input_file examples/example3.jazz --output_file out/example3.jazz
-python3 preprocessor.py --input_file examples/example4.jazz --output_file out/example4.jazz
-python3 preprocessor.py --input_file examples/example5.jazz --output_file out/example5.jazz
-python3 preprocessor.py --input_file examples/example6.jazz --output_file out/example6.jazz
+./preprocessor --input_file examples/example1.jazz --output_file out/example1.jazz
+./preprocessor --input_file examples/example2.jazz --output_file out/example2.jazz
+./preprocessor --input_file examples/example3.jazz --output_file out/example3.jazz
+./preprocessor --input_file examples/example4.jazz --output_file out/example4.jazz
+./preprocessor --input_file examples/example5.jazz --output_file out/example5.jazz
+./preprocessor --input_file examples/example6.jazz --output_file out/example6.jazz
