00f701523a489c85d27d8a47888dedbe18fa797a
Rui Fernandes <ruipedro16@protonmail.com>
2023-09-08 11:13:50 +0200

    update

--- compile_jpp_examples.sh
+++ compile_jpp_examples.sh
@@ -1,14 +1,16 @@
 #!/bin/bash 
 
+# set -e 
+
 mkdir -p jpp_examples/asm
 mkdir -p jpp_examples/jazz
 
 for file in jpp_examples/*.jpp; do
     filename=$(basename "$file" .jpp)
-    ./preprocessor --input_file $file --output_file "jpp_examples/jazz/$filename.jazz"
+    ./preprocessor -d --input_file $file --output_file "jpp_examples/jazz/$filename.jazz"
 done
 
 for file in jpp_examples/jazz/*; do
-    filename=$(basename "$file" .jpp)
-    jasminc $file -o "jpp_examples/asm/$filename.s" || (echo "Failed compiling $file" ; exit 1)
+    filename=$(basename "$file" .jazz)
+    jasminc $file -o "jpp_examples/asm/$filename.s"
 done
